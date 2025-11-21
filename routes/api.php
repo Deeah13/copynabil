78c85f7966aec32c7b225afb26149a132b2fd431
Diah Ayu Nur Rahmadani <deeahayuraditio@gmail.com>
2025-11-21 18:35:39 +0700

    Add guru schedule and material workflows

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DashboardOrangTuaController; // <--- Pastikan di-import
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BeritaController;
+use App\Http\Controllers\Api\GuruJadwalMateriController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -31,4 +32,11 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     Route::get('/catatan-guru-lengkap', [DashboardOrangTuaController::class, 'getAllCatatan']);
     Route::get('/jadwal-lengkap', [DashboardOrangTuaController::class, 'getAllJadwal']); // <--- INI YANG PENTING
+
+    // Rute Guru - Jadwal & Materi
+    Route::get('/guru/jadwal-materi', [GuruJadwalMateriController::class, 'index']);
+    Route::post('/guru/jadwal', [GuruJadwalMateriController::class, 'store']);
+    Route::put('/guru/jadwal/{jadwalSesi}', [GuruJadwalMateriController::class, 'update']);
+    Route::post('/guru/jadwal/{jadwalSesi}/materi', [GuruJadwalMateriController::class, 'storeMaterial']);
+    Route::put('/guru/materi/{materi}', [GuruJadwalMateriController::class, 'updateMaterial']);
 });
